706ae306dc0552ba86124911c38f178a67488375
Davide Gadioli <davide.gadioli@polimi.it>
2018-03-28 13:52:17 +0200

    [BENCHMARK] Updated the benchmark application

--- extra/benchmark/main.cc
+++ extra/benchmark/main.cc
@@ -1,62 +1,75 @@
 #include <iostream>
 #include <cstdlib>
+#include <chrono>
 
-#include "bench_base.hpp"
-#include "benchmark_knowledge_base.hpp"
-#include "benchmark_state.hpp"
-#include "benchmark_asrtm.hpp"
+#include "evaluator.hpp"
 
+#include "test_add_ops.hpp"
+#include "test_select_data_feature.hpp"
+#include "test_add_constraint.hpp"
+#include "test_set_rank.hpp"
+#include "test_update.hpp"
+#include "test_update_scaling.hpp"
 
 
 int main(int argc, char* argv[])
 {
 	std::cout << "Running framework benchmark:" << std::endl;
 
+  // test adding Operating Points
+  {
+    std::cout << "\tAdd Operating Points" << std::endl;
+    Launcher<std::chrono::microseconds> l("add_ops");
+    l.run< AddOperatingPoints<0> >("No Constraints");
+    l.run< AddOperatingPoints<1> >("1 Constraint");
+    l.run< AddOperatingPoints<2> >("2 Constraints");
+    l.run< AddOperatingPoints<3> >("3 Constraints");
+  }
 
-	// Perform the benchmark on the kb creation
-	benchmark_t b1("knowledge_base_creation");
-	b1(bench_knowledge_creation);
+  // test selecting a feature cluster
+  {
+    std::cout << "\tSelect feature cluster" << std::endl;
+    Launcher<std::chrono::microseconds> l("select_features");
+    l.run< SelectDataFeature<margot::FeatureDistanceType::EUCLIDEAN> >("Euclidean");
+    l.run< SelectDataFeature<margot::FeatureDistanceType::NORMALIZED> >("Normalized");
+  }
 
-	// Perform the benchmark on creating a view
-	benchmark_t b2("knowledge_base_view_creation");
-	b2(bench_view_creation);
+  // test adding a constraint
+  {
+    std::cout << "\tAdd a constraint" << std::endl;
+    Launcher<std::chrono::microseconds> l("add_constraint");
+    l.run< AddConstraint<true> >("Worst case");
+    l.run< AddConstraint<false> >("Best case");
+  }
 
-	// Perform the benchmark on adding OPS with three views
-	benchmark_t b3("knowledge_base_add_ops_3_views");
-	b3(bench_add_op);
+  // test setting the rank
+  {
+    std::cout << "\tSet the objective function" << std::endl;
+    Launcher<std::chrono::microseconds> l("set_rank");
+    l.run< SetRank<true> >("Worst case");
+    l.run< SetRank<false> >("Best case");
+  }
 
-	// Perform the benchmark on removing OPS with three views
-	benchmark_t b4("knowledge_base_remove_ops_3_views");
-	b4(bench_remove_op);
-
-
-
-
-	// Perform the benchmark on computing the linear rank
-	benchmark_t b5("state_compute_linear_rank");
-	b5(bench_set_linear_rank_3);
-
-
-
-
-	// -----------------------
-	// Test the whole asrtm
-	// -----------------------
-
-	// Add Operating Points
-	// Create a constraint
-	benchmark_t b7("asrtm_add_constraint_random");
-	b7(asrtm_add_constraint_random);
-	benchmark_t b8("asrtm_add_constraint_equal");
-	b8(asrtm_add_constraint_equal);
-	benchmark_t b9("asrtm_add_constraint_scaling");
-	b9(asrtm_add_constraint_scaling);
-	benchmark_t b6("asrtm_add_ops");
-	b6(asrtm_add_ops);
+  // test solving the optimization problem
+  {
+    std::cout << "\tSolve the optimization problem flat" << std::endl;
+    Launcher<std::chrono::microseconds> l("update_flat");
+    l.run< UpdateFlat<0> >("No changes");
+    l.run< UpdateFlat<5> >("5 Operating Points");
+    l.run< UpdateFlat<50> >("50 Operating Points");
+    l.run< UpdateFlat<100> >("100 Operating Points");
+  }
 
 
-
-	std::cout << "All done!" << std::endl;
+  // test solving the optimization problem worst case
+  {
+    std::cout << "\tSolve the optimization problem worst case" << std::endl;
+    Launcher<std::chrono::microseconds> l("update_worst_case");
+    l.run< UpdateScaling<1> >("1 constraint");
+    l.run< UpdateScaling<2> >("2 constraints");
+    l.run< UpdateScaling<3> >("3 constraints");
+    l.run< UpdateScaling<4> >("4 constraints");
+  }
 
 	return EXIT_SUCCESS;
 }
