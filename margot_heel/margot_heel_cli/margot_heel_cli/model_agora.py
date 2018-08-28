24f13595aa44fe2b18d56f2c068b82c11024e921
Davide <gadio87@gmail.com>
2018-08-28 16:24:18 +0200

    [MARGOT_HEEL] Improved the XML parser with new information

--- margot_heel/margot_heel_cli/margot_heel_cli/model_agora.py
+++ margot_heel/margot_heel_cli/margot_heel_cli/model_agora.py
@@ -4,7 +4,7 @@ class AgoraModel:
   that is communicating with the agora remote application handler
   """
 
-  available_does = ['full_factorial']
+  available_does = ['strauss', 'dmax']
 
   def __init__( self ):
 
@@ -20,6 +20,10 @@ class AgoraModel:
     self.knobs_values = {}
     self.features_values = {}
     self.metrics_monitors = {}
+    self.doe_name = 'dmax'
+    self.number_point_per_dimension = 10
+    self.number_observations_per_point = 5
+    self.min_distance = 0.2
 
 
   def __str__(self):
@@ -30,13 +34,17 @@ class AgoraModel:
     dump_string = ''
 
     dump_string = '{0}\n\n  Agora application local handler'.format(dump_string)
-    dump_string = '{0}\n    Application Name:  {1}'.format(dump_string, self.application_name)
-    dump_string = '{0}\n    Broker url:        {1}'.format(dump_string, self.broker_url)
-    dump_string = '{0}\n    Broker username:   {1}'.format(dump_string, self.username)
-    dump_string = '{0}\n    Broker password:   {1}'.format(dump_string, self.password)
-    dump_string = '{0}\n    Message qos:       {1}'.format(dump_string, self.qos)
-    dump_string = '{0}\n    Exploring:         {1}'.format(dump_string, self.knobs_values)
-    dump_string = '{0}\n    Predicting:        {1}'.format(dump_string, self.metrics_predictors)
-    dump_string = '{0}\n    Given:             {1}'.format(dump_string, self.features_values)
+    dump_string = '{0}\n    Application Name:     {1}'.format(dump_string, self.application_name)
+    dump_string = '{0}\n    Broker url:           {1}'.format(dump_string, self.broker_url)
+    dump_string = '{0}\n    Broker username:      {1}'.format(dump_string, self.username)
+    dump_string = '{0}\n    Broker password:      {1}'.format(dump_string, self.password)
+    dump_string = '{0}\n    Message qos:          {1}'.format(dump_string, self.qos)
+    dump_string = '{0}\n    DoE:                  {1}'.format(dump_string, self.doe_name)
+    dump_string = '{0}\n    N° point x dimension: {1}'.format(dump_string, self.number_point_per_dimension)
+    dump_string = '{0}\n    N° obs. x point:      {1}'.format(dump_string, self.number_observations_per_point)
+    dump_string = '{0}\n    Minimum distance:     {1}'.format(dump_string, self.min_distance)
+    dump_string = '{0}\n    Exploring:            {1}'.format(dump_string, self.knobs_values)
+    dump_string = '{0}\n    Predicting:           {1}'.format(dump_string, self.metrics_predictors)
+    dump_string = '{0}\n    Given:                {1}'.format(dump_string, self.features_values)
 
     return dump_string
--- margot_heel/margot_heel_cli/margot_heel_cli/parse_agora.py
+++ margot_heel/margot_heel_cli/margot_heel_cli/parse_agora.py
@@ -1,6 +1,18 @@
+import sys
+
 from .parser_utility import get_elements
 from .parser_utility import get_parameter
 from . import model_agora
+
+
+def my_to_str( float_value ):
+    """
+    Utility function to convert a value in string as float or integer
+    """
+    if abs(float(int(float_value)) - float_value) < 0.000000001:
+        return str(int(float_value))
+    else:
+        return str(float_value)
 
 
 
@@ -20,6 +32,10 @@ def parse_agora( agora_xml_element, namespace = ''):
   client_cert = get_parameter(agora_xml_element, 'client_cert', required = False)
   client_key = get_parameter(agora_xml_element, 'client_key', required = False)
   qos = get_parameter(agora_xml_element, 'qos', required = False)
+  doe_name = get_parameter(agora_xml_element, 'doe_name', required = False, prefixed_values = agora_model.available_does )
+  number_point_per_dimension = get_parameter(agora_xml_element, 'number_point_per_dimension', my_target_type=int, required = False)
+  number_observations_per_point = get_parameter(agora_xml_element, 'number_observations_per_point', my_target_type=int, required = False)
+  min_distance = get_parameter(agora_xml_element, 'min_distance', my_target_type=float, required = False)
 
   # update the model
   if broker_url:
@@ -36,6 +52,18 @@ def parse_agora( agora_xml_element, namespace = ''):
       agora_model.client_key = client_key
   if qos:
       agora_model.qos = qos
+  if doe_name:
+      agora_model.doe_name = doe_name
+  if number_point_per_dimension:
+      agora_model.number_point_per_dimension = number_point_per_dimension
+  if number_observations_per_point:
+      agora_model.number_observations_per_point = number_observations_per_point
+  if min_distance:
+      if min_distance < 0 or min_distance > 1:
+          print('[LOGIC ERROR] The minimum distance parameter of the agora tag must be [0..1]')
+          print('                    Provided value: "{0}"'.format(min_distance))
+          sys.exit(-1)
+      agora_model.min_distance = min_distance
 
   # get all the knobs
   knobs_xml_elements = get_elements(agora_xml_element, 'explore', namespace = namespace, required = True )
@@ -43,9 +71,49 @@ def parse_agora( agora_xml_element, namespace = ''):
   # parse each knob to explore
   for knob_xml_element in knobs_xml_elements:
 
-    # parse the knob characteristic
+    # parse the knob name (it could be whatever)
     knob_name = get_parameter(knob_xml_element, 'knob_name')
-    knob_values = get_parameter(knob_xml_element, 'values')
+
+    # tries to guess that we have a list of values for the knob
+    knob_values = get_parameter(knob_xml_element, 'values', required = False)
+
+    # otherwise it could be a range of values
+    if not knob_values:
+
+        # parse them as a range of values
+        knob_range = get_parameter(knob_xml_element, 'range', required = False)
+
+        # make sure that we have something
+        if not knob_range:
+            print('[LOGIC ERROR] Unable to parse the knob "{0}" domain!'.format(knob_name))
+            print('                    Available attributes: "{0}"'.format('", "'.join(knob_xml_element.attrib)))
+            print('                    Required attribute "values" or "range"')
+            sys.exit(-1)
+
+        # parse the string
+        try:
+            min_value, max_value, step = knob_range.split(',')
+            min_value = float(min_value)
+            max_value = float(max_value)
+            step = float(step)
+        except ValueError as err:
+            print('[LOGIC ERROR] Unable to understand the range "{0}"!'.format(knob_range))
+            print('                    Range Syntax: "min_value,max_value,step"')
+            sys.exit(-1)
+
+        # generate the discrete knob values
+        min_value = min(min_value,max_value)
+        max_value = max(min_value,max_value)
+        knob_value_list = [ min_value ]
+        counter = min_value + step
+        while counter <= max_value:
+            knob_value_list.append(counter)
+            counter = counter + step
+
+        # join them for later processing
+        knob_values = ' '.join([my_to_str(x) for x in knob_value_list])
+
+
 
     # make sure that all the knob names are lowercase
     knob_name = knob_name.lower()
@@ -65,9 +133,47 @@ def parse_agora( agora_xml_element, namespace = ''):
   # parse each knob to explore
   for feature_xml_element in features_xml_elements:
 
-    # parse the feature characteristic
+    # parse the feature name
     feature_name = get_parameter(feature_xml_element, 'feature_name')
-    feature_values = get_parameter(feature_xml_element, 'values')
+
+    # tries to guess that we have a list of values for the feature
+    feature_values = get_parameter(feature_xml_element, 'values', required = False)
+
+    # otherwise it could be a range of values
+    if not feature_values:
+
+        # parse them as a range of values
+        feature_range = get_parameter(feature_xml_element, 'range', required = False)
+
+        # make sure that we have something
+        if not feature_range:
+            print('[LOGIC ERROR] Unable to parse the feature "{0}" domain!'.format(feature_name))
+            print('                    Available attributes: "{0}"'.format('", "'.join(feature_xml_element.attrib)))
+            print('                    Required attribute "values" or "range"')
+            sys.exit(-1)
+
+        # parse the string
+        try:
+            min_value, max_value, step = feature_range.split(',')
+            min_value = float(min_value)
+            max_value = float(max_value)
+            step = float(step)
+        except ValueError as err:
+            print('[LOGIC ERROR] Unable to understand the range "{0}"!'.format(feature_range))
+            print('                    Range Syntax: "min_value,max_value,step"')
+            sys.exit(-1)
+
+        # generate the discrete knob values
+        min_value = min(min_value,max_value)
+        max_value = max(min_value,max_value)
+        feature_value_list = [ min_value ]
+        counter = min_value + step
+        while counter <= max_value:
+            feature_value_list.append(counter)
+            counter = counter + step
+
+        # join them for later processing
+        feature_values = ' '.join([my_to_str(x) for x in feature_value_list])
 
     # make sure that all the feature names are lowercase
     feature_name = feature_name.lower()
