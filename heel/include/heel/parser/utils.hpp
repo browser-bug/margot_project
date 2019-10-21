f0c646d910d563ff01170b6401201f39e8bcde7e
Davide Gadioli <davide.gadioli@polimi.it>
2019-10-21 10:56:58 +0200

    [HEEL] Minor cosmetic changes

--- heel/include/heel/parser/utils.hpp
+++ heel/include/heel/parser/utils.hpp
@@ -25,13 +25,13 @@ inline void visit_optional(const std::string& tag_name, const boost::property_tr
 
 // this is an utility function that retrieves the value of a node as a string, if it exists. If the node does
 // not exist, it returns an empty string
-inline std::string get( const std::string& tag_name, const boost::property_tree::ptree& node) {
-	const boost::optional<const boost::property_tree::ptree&> child = node.get_child_optional(tag_name);
-	if (child) {
-		return child->get<std::string>("","");
-	} else {
-		return "";
-	}
+inline std::string get(const std::string& tag_name, const boost::property_tree::ptree& node) {
+  const boost::optional<const boost::property_tree::ptree&> child = node.get_child_optional(tag_name);
+  if (child) {
+    return child->get<std::string>("", "");
+  } else {
+    return "";
+  }
 }
 
 // this is an utility function that generates a list of values (as strings), starting from a range node
