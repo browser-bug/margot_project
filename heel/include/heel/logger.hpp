74db6e496d42c21de136932978b03a52cb7d8d7f
Davide Gadioli <davide.gadioli@polimi.it>
2019-10-23 13:36:43 +0200

    [HEEL] Introduced a simple logger mechanism

---
+++ heel/include/heel/logger.hpp
@@ -0,0 +1,50 @@
+#ifndef HEEL_LOGGER_HDR
+#define HEEL_LOGGER_HDR
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace margot {
+namespace heel {
+
+class line_formatter {
+  std::ostringstream stream;
+
+ public:
+  template <class... T>
+  inline std::string format(const std::string log_str, const T &... arguments) {
+    stream << "[" << log_str << "] ";
+    insert(arguments...);
+    return stream.str();
+  }
+
+ private:
+  template <class T, class... Y>
+  inline void insert(const T &argument, const Y &... remainder) {
+    stream << argument;
+    insert(remainder...);
+  }
+
+  template <class T>
+  inline void insert(const T &argument) {
+    stream << argument << std::endl;
+  }
+
+  inline void insert(void) { stream << std::endl; }
+};
+
+template <class... T>
+inline void info(const T &... arguments) {
+  std::cout << line_formatter{}.format("   INFO", arguments...);
+}
+
+template <class... T>
+inline void warning(const T &... arguments) {
+  std::cout << line_formatter{}.format("WARNING", arguments...);
+}
+
+}  // namespace heel
+}  // namespace margot
+
+#endif  // HEEL_LOGGER_HDR
