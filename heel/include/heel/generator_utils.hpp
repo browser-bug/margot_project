9a7cf46ec9c93dab5d566f654c4082cac92779fc
Davide Gadioli <davide.gadioli@polimi.it>
2019-11-04 12:49:52 +0100

    [HEEL] Bugfix: the join function must converts the string in-order

--- heel/include/heel/generator_utils.hpp
+++ heel/include/heel/generator_utils.hpp
@@ -11,17 +11,17 @@ namespace heel {
 
 // this utility function performs the equivalent of "''.join()" in python, but over an iterable of objects not
 // only strings. The conversion from the object to a string is performed by the given functor.
+// NOTE: we can't put the oneliner version, since we need to print the first element first
 template <class iterator_type, class functor_type>
 inline std::string join(const iterator_type& begin, const iterator_type& end, const std::string& separator,
                         const functor_type& functor) {
-  return begin == end
-             ? std::string{}
-             : functor(*begin) + std::accumulate(std::next(begin), end, std::string{},
-                                                 [&separator, &functor](
-                                                     const std::string& partial_result,
-                                                     const typename iterator_type::value_type& new_element) {
-                                                   return partial_result + separator + functor(new_element);
-                                                 });
+  std::string string_initial_element = begin == end ? std::string() : functor(*begin);
+  return string_initial_element +
+         std::accumulate(std::next(begin), end, std::string{},
+                         [&separator, &functor](const std::string& partial_result,
+                                                const typename iterator_type::value_type& new_element) {
+                           return partial_result + separator + functor(new_element);
+                         })
 }
 
 // this struct represents a piece of content in cpp source/header file, plus all the headers that it requires
