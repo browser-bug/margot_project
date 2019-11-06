16729d45de063bb552eaefa20893457f19f38750
Davide Gadioli <davide.gadioli@polimi.it>
2019-11-06 17:29:40 +0100

    [SCRIPTS] Now the build scripts will install everything in a given folder

--- scripts/get_deps_path.sh
+++ scripts/get_deps_path.sh
@@ -2,14 +2,14 @@
 
 # get the root path of the project
 if git rev-parse --git-dir > /dev/null 2>&1; then :
-	PROJECT_ROOT=$(git rev-parse --show-toplevel)
+	GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
 else :
 	>&2 echo "Error: you must execute this script in the benchmark repository"
 	exit -1
 fi
 
 # load all the paths for margot
-source $PROJECT_ROOT/etc/path.env
+source $GIT_REPO_ROOT/etc/path.env
 
 # print the install folder of the external dependencies
-echo -n $PROJECT_ROOT/$DEPS_INSTALL_PATH
+echo -n $DEPS_INSTALL_PATH
