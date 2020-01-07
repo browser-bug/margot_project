69c8b60e739273267b9ac5dddd57f072b123efea
Davide Gadioli <davide.gadioli@polimi.it>
2020-01-07 15:18:41 +0100

    [HEEL] Bugfix: strings should be passed by reference

--- heel/include/heel/cpp_utils.hpp
+++ heel/include/heel/cpp_utils.hpp
@@ -11,13 +11,13 @@ namespace heel {
 
 // this helper function generate the cpp code that translate a field "name" to its index
 inline std::string generate_field_getter(const subject_kind kind, const std::string& field_name,
-                                         const std::string block_name) {
+                                         const std::string& block_name) {
   const std::string section_name = kind == subject_kind::METRIC ? std::string("metric") : std::string("knob");
   return "static_cast<size_t>(margot::" + block_name + "::" + section_name + "::" + field_name + ")";
 }
 
 // this function generates the goal cpp identifier that must be used in the generated code
-inline std::string generate_goal_identifier(const std::string state_name, const std::size_t id) {
+inline std::string generate_goal_identifier(const std::string& state_name, const std::size_t id) {
   return state_name + "_constraint_" + std::to_string(id);
 }
 
