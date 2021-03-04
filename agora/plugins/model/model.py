e734931461ce366d804148c785bab92089417bb2
Bernardo Menicagli <bernardo.menicagli@gmail.com>
2021-03-04 17:47:13 +0100

    [AGORA] Added TargetTransformRegressor + minors

--- agora/plugins/model/model.py
+++ agora/plugins/model/model.py
@@ -2,14 +2,18 @@ import json
 import numpy as np
 from sklearn.base import RegressorMixin
 from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.model_selection import cross_validate, cross_val_score, KFold, LeavePOut
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import cross_validate, KFold, LeavePOut
+from sklearn.preprocessing import PolynomialFeatures, PowerTransformer, StandardScaler
+from sklearn.compose import TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 
 # testing
 from sklearn.datasets import make_regression
 
-def create_model(model_params, data, target):
+# stats
+from statistics import store_stats
+
+def create_model(metric_name, num_iterations, model_params, data, target):
     print(model_params)
     algorithm = model_params['algorithm'] if 'algorithm' in model_params.keys() else 'linear'
     hyper_parameters = json.loads(model_params['hyper_parameters'].replace("'", "\"")) if 'hyper_parameters' in model_params.keys() else {}
@@ -19,15 +23,18 @@ def create_model(model_params, data, target):
     estimator = RegressorMixin()
     if algorithm == 'linear':
         print("Using an ordinary least squares linear regression estimator")
-        estimator = Pipeline([('poly', PolynomialFeatures(degree=5)), ('linear', LinearRegression())])
+        estimator = Pipeline([('poly', PolynomialFeatures(degree=5)), ('linear', LinearRegression(normalize=True))])
     elif algorithm == 'ridge':
         print("Using a Ridge regression estimator")
         alpha = float(hyper_parameters['alpha']) if 'alpha' in hyper_parameters.keys() else 1.0
         solver = hyper_parameters['solver'] if 'solver' in hyper_parameters.keys() else 'auto'
-        estimator = Pipeline([('poly', PolynomialFeatures(degree=5)), ('linear', Ridge(alpha=alpha,solver=solver))])
+        estimator = Pipeline([('poly', PolynomialFeatures(degree=5)), ('ridge', Ridge(alpha=alpha,normalize=True,solver=solver))])
     else:
         print("Unknown estimator name, returning empty object.")
         return False, estimator
+
+    # apply the target transformation
+    estimator = TransformedTargetRegressor(regressor=estimator, transformer=PowerTransformer())
 
     # produce the cross validation
     num_cv_folds = int(model_params['num_cv_folds']) if 'num_cv_folds' in model_params.keys() else 5
@@ -48,15 +55,23 @@ def create_model(model_params, data, target):
         # print(data.loc[test])
     ########
 
-    cv_results = cross_validate(estimator, data, target, cv=splits_iterator, scoring=list(scoring_thresholds.keys()), verbose=1, n_jobs=-1)
+    scores_list = list(scoring_thresholds.keys())
+    cv_results = cross_validate(estimator, data, target, cv=splits_iterator,return_estimator=True, scoring=scores_list, verbose=1, n_jobs=-1)
     print(cv_results)
 
+    # store statistics on file
+    store_stats(metric_name, num_iterations, cv_results, data, target)
+
     # check if the score thresholds are verified
+    best_estimator = [0] * num_cv_folds
+    is_model_good = True
     for scoring, threshold in scoring_thresholds.items():
+        scores_max_pos = np.argmax(cv_results['test_' + scoring])
+        best_estimator[scores_max_pos] += 1
         scores_mean = np.mean(cv_results['test_' + scoring])
         print("Comparing "+scoring,cv_results['test_'+scoring]," MEAN[",scores_mean,"] with",threshold)
         if not scores_mean >= threshold:
             print("The scoring", scoring, "has not verified the threshold limit.")
-            return False, estimator
+            is_model_good = False
     # since the model is good we can fit it
-    return True, estimator
+    return is_model_good, cv_results['estimator'][np.argmax(best_estimator)]
